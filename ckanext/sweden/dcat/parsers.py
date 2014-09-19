58fb177106800fbef4aa77e739e5506d9a5ae587
amercader <amercadero@gmail.com>
2014-09-19 12:04:11 +0100

    [#1] Move parser to class so it can be extended

--- ckanext/sweden/dcat/parsers.py
+++ ckanext/sweden/dcat/parsers.py
@@ -2,10 +2,11 @@
 
 # Dev stuff, will probably get removed
 import fileinput
+
 import json
 
 import rdflib
-from rdflib.namespace import Namespace
+from rdflib.namespace import Namespace, RDF
 
 DCT = Namespace("http://purl.org/dc/terms/")
 DCAT = Namespace("http://www.w3.org/ns/dcat#")
@@ -13,181 +14,156 @@ ADMS = Namespace("http://www.w3.org/ns/adms#")
 VCARD = Namespace("http://www.w3.org/2006/vcard/ns#")
 FOAF = Namespace("http://xmlns.com/foaf/0.1/")
 
-g = rdflib.Graph()
+
+class RDFParser(object):
+
+    def __init__(self):
+
+        self.g = rdflib.Graph()
+
+    def _datasets(self):
+        '''
+        Returns all DCAT datasets on the graph
+        '''
+        for dataset in self.g.subjects(RDF.type, DCAT.Dataset):
+            yield dataset
+
+    def _distributions(self, dataset):
+
+        for distribution in self.g.objects(dataset, DCAT.distribution):
+            yield distribution
+
+    def _object(self, subject, predicate):
+
+        for o in self.g.objects(subject, predicate):
+            return unicode(o)
+        return None
+
+    def _object_int(self, subject, predicate):
+
+        object_value = self._object(subject, predicate)
+        if object_value:
+            try:
+                return int(object_value)
+            except ValueError:
+                pass
+        return None
+
+    def _object_list(self, subject, predicate):
+
+        return [unicode(o) for o in self.g.objects(subject, predicate)]
+
+    def parse(self, contents, _format=None):
+
+        raise NotImplemented
 
 
-query_datasets = '''
-SELECT ?dataset
-WHERE {
-    ?dataset a dcat:Dataset
-}
-'''
+class DCATAPParser(RDFParser):
 
-query_datasets2 = '''
-SELECT ?dataset
-WHERE {
-    ?catalog dcat:dataset ?dataset
-}
-'''
+    def parse(self, contents, _format=None):
 
-query_distributions = '''
-SELECT ?distribution
-WHERE {
-    ?dataset dcat:distribution ?distribution
-}
-'''
+        self.g.parse(data=contents, format=_format)
 
+        ckan_datasets = []
 
-def _datasets():
-    '''
-    Returns all DCAT datasets on the graph
-    '''
+        for dataset in self._datasets():
 
-    datasets = [r.dataset for r in g.query(query_datasets)]
+            ckan_dict = {
+                'tags': [],
+                'extras': [],
+                'resources': []
+            }
 
-    if not datasets:
+            # Basic fields
+            for key, predicate in (
+                    ('title', DCT.title),
+                    ('notes', DCT.description),
+                    ('url', DCAT.landingUrl),
+                    ):
+                value = self._object(dataset, predicate)
+                if value:
+                    ckan_dict[key] = value
 
-        datasets = [r.dataset for r in g.query(query_datasets2)]
+            # Tags
 
-    return datasets
+            keywords = self._object_list(dataset, DCAT.keyword) or []
+            for keyword in keywords:
+                ckan_dict['tags'].append({'name': keyword})
 
-
-def _distributions(dataset):
-
-    for r in g.query(query_distributions, initBindings={'dataset': dataset}):
-        yield r.distribution
-
-
-def _object(subject, predicate):
-
-    objects = [unicode(o) for o in g.objects(subject, predicate)]
-
-    return objects[0] if objects else None
-
-
-def _object_int(subject, predicate):
-
-    object_value = _object(subject, predicate)
-    if object_value:
-        try:
-            return int(object_value)
-        except ValueError:
-            pass
-    return None
-
-
-def _object_list(subject, predicate):
-
-    objects = [unicode(o) for o in g.objects(subject, predicate)]
-
-    return objects or []
-
-
-def parse(contents):
-
-    g.parse(data=contents)
-
-    g.bind('dct', 'http://purl.org/dc/terms/')
-    g.bind('dcat', 'http://www.w3.org/ns/dcat#')
-
-    ckan_datasets = []
-    for dataset in _datasets():
-
-        ckan_dict = {
-            'tags': [],
-            'extras': [],
-            'resources': []
-        }
-
-        # Basic fields
-        for key, predicate in (
-                ('title', DCT.title),
-                ('notes', DCT.description),
-                ('url', DCAT.landingUrl),
-                ):
-            value = _object(dataset, predicate)
-            if value:
-                ckan_dict[key] = value
-
-        # Tags
-
-        keywords = _object_list(dataset, DCAT.keyword) or []
-        for keyword in keywords:
-            ckan_dict['tags'].append({'name': keyword})
-
-        # Extras
-
-        #  Simple values
-        for key, predicate in (
-                ('dcat_issued', DCT.issued),
-                ('dcat_modified', DCT.modified),
-                ('guid', DCT.identifier),  # backwards compatibility
-                ('dcat_identifier', DCT.identifier),
-                ('dcat_alternate_identifier', ADMS.identifier),
-                ('dcat_version', ADMS.version),
-                ('dcat_version_notes', ADMS.versionNotes),
-                ('dcat_frequency', DCT.accrualPeriodicity),
-                ('dcat_spatial', DCT.spatial),
-                ('dcat_temporal', DCT.temporal),
-
-                ):
-            value = _object(dataset, predicate)
-            if value:
-                ckan_dict['extras'].append({'key': key, 'value': value})
-
-        #  Lists
-        for key, predicate in (
-                ('language', DCT.language),
-                ('dcat_theme', DCAT.theme),
-                ('dcat_conforms_to', DCAT.conformsTo),
-                ):
-            values = _object_list(dataset, predicate)
-            if values:
-                ckan_dict['extras'].append({'key': key, 'value': values})
-
-        # Dataset URI (explicitly show the missing ones)
-        dataset_uri = (unicode(dataset)
-                       if isinstance(dataset, rdflib.term.URIRef)
-                       else None)
-        ckan_dict['extras'].append({'key': 'uri', 'value': dataset_uri})
-
-        # Resources
-        for distribution in _distributions(dataset):
-
-            resource_dict = {}
+            # Extras
 
             #  Simple values
             for key, predicate in (
-                    ('name', DCT.title),
-                    ('description', DCT.description),
-                    ('format', DCT.format),
-                    ('mimetype', DCAT.mediaType),
-                    ('dcat_download_url', DCAT.downloadURL),
                     ('dcat_issued', DCT.issued),
                     ('dcat_modified', DCT.modified),
-                    ('dcat_status', ADMS.status),
-                    ('dcat_rights', DCT.rights),
-                    ('dcat_licence', DCT.license),
+                    ('guid', DCT.identifier),  # backwards compatibility
+                    ('dcat_identifier', DCT.identifier),
+                    ('dcat_alternate_identifier', ADMS.identifier),
+                    ('dcat_version', ADMS.version),
+                    ('dcat_version_notes', ADMS.versionNotes),
+                    ('dcat_frequency', DCT.accrualPeriodicity),
+                    ('dcat_spatial', DCT.spatial),
+                    ('dcat_temporal', DCT.temporal),
+
                     ):
-                value = _object(distribution, predicate)
+                value = self._object(dataset, predicate)
                 if value:
-                    resource_dict[key] = value
+                    ckan_dict['extras'].append({'key': key, 'value': value})
 
-            resource_dict['url'] = (_object(distribution, DCAT.accessURL) or
-                                    _object(distribution, DCAT.downloadURL))
+            #  Lists
+            for key, predicate in (
+                    ('language', DCT.language),
+                    ('dcat_theme', DCAT.theme),
+                    ('dcat_conforms_to', DCAT.conformsTo),
+                    ):
+                values = self._object_list(dataset, predicate)
+                if values:
+                    ckan_dict['extras'].append({'key': key, 'value': values})
 
-            size = _object_int(distribution, DCAT.byteSize)
-            if size is not None:
-                    resource_dict['size'] = size
+            # Dataset URI (explicitly show the missing ones)
+            dataset_uri = (unicode(dataset)
+                           if isinstance(dataset, rdflib.term.URIRef)
+                           else None)
+            ckan_dict['extras'].append({'key': 'uri', 'value': dataset_uri})
 
-            # Distribution URI (explicitly show the missing ones)
-            resource_dict['uri'] = (unicode(distribution)
-                                    if isinstance(distribution, rdflib.term.URIRef)
-                                    else None)
+            # Resources
+            for distribution in self._distributions(dataset):
 
-            ckan_dict['resources'].append(resource_dict)
+                resource_dict = {}
 
-        ckan_datasets.append(ckan_dict)
+                #  Simple values
+                for key, predicate in (
+                        ('name', DCT.title),
+                        ('description', DCT.description),
+                        ('format', DCT.format),
+                        ('mimetype', DCAT.mediaType),
+                        ('dcat_download_url', DCAT.downloadURL),
+                        ('dcat_issued', DCT.issued),
+                        ('dcat_modified', DCT.modified),
+                        ('dcat_status', ADMS.status),
+                        ('dcat_rights', DCT.rights),
+                        ('dcat_licence', DCT.license),
+                        ):
+                    value = self._object(distribution, predicate)
+                    if value:
+                        resource_dict[key] = value
+
+                resource_dict['url'] = (self._object(distribution, DCAT.accessURL) or
+                                        self._object(distribution, DCAT.downloadURL))
+
+                size = self._object_int(distribution, DCAT.byteSize)
+                if size is not None:
+                        resource_dict['size'] = size
+
+                # Distribution URI (explicitly show the missing ones)
+                resource_dict['uri'] = (unicode(distribution)
+                                        if isinstance(distribution,
+                                                      rdflib.term.URIRef)
+                                        else None)
+
+                ckan_dict['resources'].append(resource_dict)
+
+            ckan_datasets.append(ckan_dict)
 
         return ckan_datasets
 
@@ -198,6 +174,7 @@ if __name__ == '__main__':
     for line in fileinput.input():
         contents += line
 
-    ckan_datasets = parse(contents)
+    parser = DCATAPParser()
+    ckan_datasets = parser.parse(contents)
 
     print json.dumps(ckan_datasets)
