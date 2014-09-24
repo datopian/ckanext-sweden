e806f6a3e44d33e6c6f6df94a29619773a1de1dd
John Martin <me@johnmart.in>
2014-09-24 13:24:30 +0100

    [#8] Added helpers and polished off first proper version of homepage

--- ckanext/sweden/theme/plugin.py
+++ ckanext/sweden/theme/plugin.py
@@ -1,4 +1,30 @@
 import ckan.plugins as p
+from ckan import model
+
+def _get_datasets(sort):
+  context = {'model': model, 'session': model.Session,
+             'user': p.toolkit.c.user, 'for_view': True,
+             'auth_user_obj': p.toolkit.c.userobj}
+  data_dict = {'rows': 3, 'start': 0, 'sort': sort}
+  query = p.toolkit.get_action('package_search')(context, data_dict)
+  if (query['results']):
+    return query['results']
+  return False
+
+def get_most_viewed_datasets():
+  return _get_datasets('views desc')
+
+def get_recently_updated_datasets():
+    return _get_datasets('metadata_modified desc')
+
+def get_top_groups():
+  return
+
+def get_recent_blog_posts():
+    from ckanext.sweden.blog.model.post import Post
+    posts = model.Session.query(Post).\
+      filter(Post.visible == True).order_by('created desc').limit(3)
+    return posts
 
 class ThemePlugin(p.SingletonPlugin):
     """This extension adds the ckanext-theme to ckan
@@ -10,6 +36,15 @@ class ThemePlugin(p.SingletonPlugin):
     """
     p.implements(p.IConfigurer, inherit=True)
     p.implements(p.IConfigurable, inherit=True)
+    p.implements(p.ITemplateHelpers, inherit=False)
+
+    def get_helpers(self):
+        return {
+          'get_most_viewed_datasets': get_most_viewed_datasets,
+          'get_recently_updated_datasets': get_recently_updated_datasets,
+          'get_top_groups': get_top_groups,
+          'get_recent_blog_posts': get_recent_blog_posts
+        }
 
     def update_config(self, config):
         ''' Set up template, public and fanstatic directories
