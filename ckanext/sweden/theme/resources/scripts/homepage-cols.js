d89161b483f68651cbf8299a13450398d41d1691
John Martin <me@johnmart.in>
2014-09-30 12:38:14 +0100

    [#8] Removed uneeded proxyAll from homepage-cols

--- ckanext/sweden/theme/resources/scripts/homepage-cols.js
+++ ckanext/sweden/theme/resources/scripts/homepage-cols.js
@@ -1,7 +1,6 @@
 this.ckan.module('homepage-cols', function($, _) {
   return {
     initialize: function () {
-      $.proxyAll(this, /_on/);
       $('.span4 .box', this.el).matchHeight();
     }
   };
