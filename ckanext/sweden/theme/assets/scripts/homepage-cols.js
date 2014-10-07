8870df3ec7e603775052fc252b245a59f554a7c0
John Martin <me@johnmart.in>
2014-10-07 11:17:38 +0100

    Adds tweets to homepage

--- ckanext/sweden/theme/assets/scripts/homepage-cols.js
+++ ckanext/sweden/theme/assets/scripts/homepage-cols.js
@@ -1,8 +1,24 @@
 (function(ckan) {
   ckan.module('homepage-cols', function($, _) {
+    function ResizeIframe () {
+      var resized = false;
+      var check = ( $('iframe.twitter-timeline', this.el).length == 1 );
+      if (check) {
+        var iframe = $('iframe.twitter-timeline', this.el);
+        if (iframe.outerHeight() > 0) {
+          var height = $('.span4 .box:first', this.el).outerHeight()-80;
+          iframe.css('height', height);
+          resized = true;
+        }
+      }
+      if (!resized) {
+        setTimeout(ResizeIframe, 200);
+      }
+    }
     return {
       initialize: function () {
         $('.span4 .box', this.el).matchHeight();
+        ResizeIframe();
       }
     };
   });
