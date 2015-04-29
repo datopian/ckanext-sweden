72d30c38c8300b05ded9490ed7fe18f0db1d4054
Brook Elgie <specialbrew@gmail.com>
2015-04-29 17:00:07 +0100

    Change eurovoc facet position in facet list

--- ckanext/sweden/plugin.py
+++ ckanext/sweden/plugin.py
@@ -8,6 +8,7 @@ class SwedenPlugin(plugins.SingletonPlugin):
 
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IPackageController, inherit=True)
+    plugins.implements(plugins.IFacets)
 
     # IConfigurer
 
@@ -31,3 +32,36 @@ class SwedenPlugin(plugins.SingletonPlugin):
                 theme_list = [theme]
             pkg_dict['eurovoc_category'] = theme_list[0]
         return pkg_dict
+
+    # IFacets
+
+    def dataset_facets(self, facets_dict, package_type):
+        facets_dict = self._update_facets(facets_dict)
+        return facets_dict
+
+    def group_facets(self, facets_dict, group_type, package_type):
+        facets_dict = self._update_facets(facets_dict)
+        return facets_dict
+
+    def organization_facets(self, facets_dict, organization_type, package_type):
+        facets_dict = self._update_facets(facets_dict)
+        return facets_dict
+
+    def _update_facets(self, facets_dict):
+        '''Add `eurovoc_category_label` and change its position.'''
+
+        new_position = 1  # new zero indexed position
+        eurovoc_facet = {'eurovoc_category_label':
+                         plugins.toolkit._('Eurovoc Categories')}
+
+        if 'eurovoc_category_label' in facets_dict:
+            del facets_dict['eurovoc_category_label']
+
+        old_facets = facets_dict.copy()
+        facets_dict.clear()
+        for i, facet in enumerate(old_facets):
+            if i == new_position:
+                facets_dict.update(eurovoc_facet)
+            facets_dict.update({facet: old_facets[facet]})
+
+        return facets_dict
