cff4ed51883d78c8c90c5ba6b7e6dc5199ba3427
Brook Elgie <specialbrew@gmail.com>
2015-05-06 12:21:53 +0100

    Remove  arg

--- ckanext/sweden/dcat/plugin.py
+++ ckanext/sweden/dcat/plugin.py
@@ -29,7 +29,7 @@ class SwedenDCATRDFHarvester(p.SingletonPlugin):
 
         errors = []
         try:
-            r = requests.post(validation_service, data=content, verify=False)
+            r = requests.post(validation_service, data=content)
         except requests.exceptions.RequestException, e:
             errors.append('Error contacting the validation service: {0}'.format(str(e)))
 
